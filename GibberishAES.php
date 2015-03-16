26739a70f02c4e43cc7f5119fa5ba18c58d85968
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-03-16 21:17:21 +0200

    Adding a code fragment for random bytes generation.

--- GibberishAES.php
+++ GibberishAES.php
@@ -70,8 +70,8 @@ class GibberishAES {
     protected static $openssl_cli_exists = null;
 
     // This is a static class, instances are disabled.
-    final private function __construct() { }
-    final private function __clone() { }
+    final private function __construct() {}
+    final private function __clone() {}
 
     /**
      * Crypt AES (256, 192, 128)
@@ -309,18 +309,133 @@ class GibberishAES {
             }
         }
 
-        // Borrowed from http://phpseclib.com/
+        // Rename the parameter on order it to fit with the code below.
+        $len = $length;
 
-        $rnd = '';
+        /*
+         * The following code fragment has been taken from Secure-random-bytes-in-PHP
+         * project, released under the New BSD License.
+         * @see https://github.com/ivantcholakov/Secure-random-bytes-in-PHP
+         *
+         *
+         *
+         * Author:
+         * George Argyros <argyros.george@gmail.com>
+         *
+         * Copyright (c) 2012, George Argyros
+         * All rights reserved.
+         *
+         * Redistribution and use in source and binary forms, with or without
+         * modification, are permitted provided that the following conditions are met:
+         *    * Redistributions of source code must retain the above copyright
+         *      notice, this list of conditions and the following disclaimer.
+         *    * Redistributions in binary form must reproduce the above copyright
+         *      notice, this list of conditions and the following disclaimer in the
+         *      documentation and/or other materials provided with the distribution.
+         *    * Neither the name of the <organization> nor the
+         *      names of its contributors may be used to endorse or promote products
+         *      derived from this software without specific prior written permission.
+         *
+         * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+         * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+         * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+         * DISCLAIMED. IN NO EVENT SHALL GEORGE ARGYROS BE LIABLE FOR ANY
+         * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+         * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+         * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+         * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+         * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+         * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+         *
+         *
+         *
+         * The function is providing, at least at the systems tested :),
+         * $len bytes of entropy under any PHP installation or operating system.
+         * The execution time should be at most 10-20 ms in any system.
+         */
 
-        for ($i = 0; $i < $length; $i++) {
+        $SSLstr = '4'; // http://xkcd.com/221/
 
-            $sha = hash('sha256', mt_rand());
-            $char = mt_rand(0, 30);
-            $rnd .= chr(hexdec($sha[$char].$sha[$char + 1]));
+        /*
+         * No build-in crypto randomness function found. We collect any entropy
+         * available in the PHP core PRNGs along with some filesystem info and memory
+         * stats. To make this data cryptographically strong we add data either from
+         * /dev/urandom or if its unavailable, we gather entropy by measuring the
+         * time needed to compute a number of SHA-1 hashes.
+         */
+        $str = '';
+        $bits_per_round = 2; // bits of entropy collected in each clock drift round
+        $msec_per_round = 400; // expected running time of each round in microseconds
+        $hash_len = 20; // SHA-1 Hash length
+        $total = $len; // total bytes of entropy to collect
+        $handle = @fopen('/dev/urandom', 'rb');
+        if ($handle && function_exists('stream_set_read_buffer')) {
+            @stream_set_read_buffer($handle, 0);
         }
 
-        return $rnd;
+        do {
+            $bytes = ($total > $hash_len) ? $hash_len : $total;
+            $total -= $bytes;
+            //collect any entropy available from the PHP system and filesystem
+            $entropy = rand() . uniqid(mt_rand(), true) . $SSLstr;
+            $entropy .= implode('', @fstat(@fopen(__FILE__, 'r')));
+            $entropy .= memory_get_usage() . getmypid();
+            $entropy .= serialize($_ENV) . serialize($_SERVER);
+            if (function_exists('posix_times')) {
+                $entropy .= serialize(posix_times());
+            }
+            if (function_exists('zend_thread_id')) {
+                $entropy .= zend_thread_id();
+            }
+            if ($handle) {
+                $entropy .= @fread($handle, $bytes);
+            } else {
+                // Measure the time that the operations will take on average
+                for ($i = 0; $i < 3; $i++) {
+                    $c1 = microtime(true);
+                    $var = sha1(mt_rand());
+                    for ($j = 0; $j < 50; $j++) {
+                        $var = sha1($var);
+                    }
+                    $c2 = microtime(true);
+                    $entropy .= $c1 . $c2;
+                }
+                // Based on the above measurement determine the total rounds
+                // in order to bound the total running time.
+                $rounds = (int) ($msec_per_round * 50 / (int) (($c2 - $c1) * 1000000));
+                // Take the additional measurements. On average we can expect
+                // at least $bits_per_round bits of entropy from each measurement.
+                $iter = $bytes * (int) (ceil(8 / $bits_per_round));
+                for ($i = 0; $i < $iter; $i++) {
+                    $c1 = microtime();
+                    $var = sha1(mt_rand());
+                    for ($j = 0; $j < $rounds; $j++) {
+                        $var = sha1($var);
+                    }
+                    $c2 = microtime();
+                    $entropy .= $c1 . $c2;
+                }
+            }
+            // We assume sha1 is a deterministic extractor for the $entropy variable.
+            $str .= sha1($entropy, true);
+
+        // Modified by Ivan Tcholakov, 16-MAR-2015.
+        //} while ($len > strlen($str));
+        } while ($len > self::strlen($str));
+        //
+
+        if ($handle) {
+            @fclose($handle);
+        }
+
+        // Modified by Ivan Tcholakov, 16-MAR-2015.
+        //return substr($str, 0, $len);
+        return self::substr($str, 0, $len);
+        //
+
+        /*
+         * End of code fragment from Secure-random-bytes-in-PHP project.
+         */
     }
 
     protected static function aes_cbc_encrypt($string, $key, $iv) {
