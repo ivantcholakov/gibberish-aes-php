5eb89e5b2908cadabb275233b72ef1fd26a31744
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-11-24 20:20:54 +0200

    A new release, 1.2.1.

--- GibberishAES.php
+++ GibberishAES.php
@@ -47,7 +47,7 @@
  * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2015.
  * Code repository: @link https://github.com/ivantcholakov/gibberish-aes-php
  *
- * @version 1.2.0
+ * @version 1.2.1
  *
  * @license The MIT License (MIT)
  * @link http://opensource.org/licenses/MIT
